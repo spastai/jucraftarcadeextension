8a454fbf6bb857b8051712f87780f394baf4620c
Marijus <spastai@gmail.com>
2024-12-25 19:10:12 +0100

    Try animation

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 //% color="#AA278D" weight=100
 namespace JuCraft {
+    let steve = sprites.create(Player_013, SpriteKind.Player)
+
     //% block
     export function showSteve() {
-        let mySprite = sprites.create(Player_013, SpriteKind.Player)
+    }
+
+    //% block
+    export function walkSteveDown() {
+        animation.runImageAnimation(steve, [Player_013, Player_014, Player_014, Player_015, Player_016], 500, true)
     }
 }
